3f9b8efd0d86e8f640f9548c1d6dc5a99d9f4edb
Anosh Malik <me@ano.sh>
2016-10-15 21:21:06 +0100

    Add remaining functions to ATM

--- libs/atm.js
+++ libs/atm.js
@@ -1,9 +1,42 @@
 const request               = require('request-promise');
 module.exports = {
-  apiKey             : function() {
+  apiKey           : function() {
     return module.parent.exports.apiKey;
   },
-  urlWithEntity         : function() {
+  urlWithEntity    : function() {
     return module.parent.exports.baseUrl+'/atms/';
-  }
+  },
+  getAll           : function() {
+    return request({
+      'url'    : this.urlWithEntity(),
+      'method' :'GET',
+      'qs'     : {
+        'key' : this.apiKey()
+      },
+      'json'   : true
+    });
+  },
+  getATM           : function(id) {
+    return request({
+      'url'    : this.urlWithEntity()+id,
+      'method' :'GET',
+      'qs'     : {
+        'key' : this.apiKey()
+      },
+      'json'   : true
+    });
+  },
+  getAtmByLocation : function(lat, lng, rad) {
+    return request({
+      'url'    : this.urlWithEntity(),
+      'method' :'GET',
+      'qs'     : {
+        'key' : this.apiKey(),
+        'lat' : lat,
+        'lng' : lng,
+        'rad' : rad
+      },
+      'json'   : true
+    });
+  },
 };
